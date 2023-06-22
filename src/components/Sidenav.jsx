5a52acafaa8e8963bd656a06806887c299810a05
Raymarth <raymarthipsagel@gmail.com>
2023-06-22 16:05:14 +0800

    Add header navbar

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -17,7 +17,7 @@ const Sidenav = () => {
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] md:hidden"
+        className="absolute top-4 right-4 z-[99] md:hidden lg:hidden"
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
@@ -58,7 +58,7 @@ const Sidenav = () => {
         ""
       )}
 
-      <div className="md:block hidden fixed top-[25%] z-10">
+      <div className="md:block hidden lg:hidden fixed top-[25%] z-10">
         <div className="flex flex-col">
           <a
             href="#Home"
@@ -83,6 +83,47 @@ const Sidenav = () => {
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineProject size={20} />
+          </a>
+        </div>
+      </div>
+
+      <div className="lg:block hidden fixed top-[10%] right-[5%] z-10">
+        <div className="flex h-12">
+          <a
+            href="#Home"
+            className="rounded shadow-sm hover:bg-gray-100 hover:shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <div className="flex p-3">
+              <AiOutlineHome size={20} className="mr-2" />
+              <span>Home</span>
+            </div>
+          </a>
+          <a
+            href="#Profile"
+            className="rounded shadow-sm hover:bg-gray-100 hover:shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <div className="flex p-3">
+              <AiOutlineUser size={20} className="mr-2" />
+              <span>Profile</span>
+            </div>
+          </a>
+          <a
+            href="#Experience"
+            className="rounded shadow-sm hover:bg-gray-100 hover:shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <div className="flex p-3">
+              <AiOutlineAppstore size={20} className="mr-2" />
+              <span>Experience</span>
+            </div>
+          </a>
+          <a
+            href="#Projects"
+            className="rounded shadow-sm hover:bg-gray-100 hover:shadow-gray-400 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <div className="flex p-3">
+              <AiOutlineProject size={20} className="mr-2" />
+              <span>Projects</span>
+            </div>
           </a>
         </div>
       </div>
