d4dd3a10e6cf488ebe83b37408728612f325f73b
Raymarth <raymarthipsagel@gmail.com>
2023-06-30 09:26:19 +0800

    Fixed the pictures details

--- src/components/ProjectsItems.jsx
+++ src/components/ProjectsItems.jsx
@@ -5,12 +5,14 @@ const ProjectsItems = ({ img, title }) => {
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]">
       <img src={img} alt="/" className="rounded-xl group-hover:opacity-10" />
       <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <h3 className="text-2xl font-bold text-white tracking-wider text-ceter">
+        <h3 className="lg:text-2xl text-[15px] font-bold text-white tracking-wider text-ceter">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">Vb6.0 | MySQL</p>
+        <p className="pb-4 pt-2 lg:text-md text-sm text-white text-center">
+          Vb6.0 | MySQL
+        </p>
         <a href="/">
-          <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg">
+          <p className="text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer lg:text-lg text-md">
             More Info
           </p>
         </a>
